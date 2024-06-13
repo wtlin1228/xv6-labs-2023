419eea2543ea20eeddb4f5824724fa17a8f8bd22
wtlin1228 <wtlin1228@gmail.com>
2024-06-14 01:59:04 +0800

    pass lab3 - print a page table

--- kernel/vm.c
+++ kernel/vm.c
@@ -449,3 +449,43 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+
+  // level 1 pagetable
+  for (int i = 0; i < 512; i++)
+  {
+    pte_t pte = pagetable[i];
+    if ((pte & PTE_V) && (pte & (PTE_R | PTE_W | PTE_X)) == 0)
+    {
+      uint64 pa = PTE2PA(pte);
+      printf("..%d: pte %p pa %p\n", i, pte, pa);
+
+      // level 2 pagetable
+      pagetable_t pagetable = (pagetable_t)pa;
+      for (int i = 0; i < 512; i++)
+      {
+        pte_t pte = pagetable[i];
+        if ((pte & PTE_V) && (pte & (PTE_R | PTE_W | PTE_X)) == 0)
+        {
+          uint64 pa = PTE2PA(pte);
+          printf(".. ..%d: pte %p pa %p\n", i, pte, pa);
+
+          // level 3 pagetable
+          pagetable_t pagetable = (pagetable_t)pa;
+          for (int i = 0; i < 512; i++)
+          {
+            pte_t pte = pagetable[i];
+            if (pte & PTE_V)
+            {
+              uint64 pa = PTE2PA(pte);
+              printf(".. .. ..%d: pte %p pa %p\n", i, pte, pa);
+            }
+          }
+        }
+      }
+    }
+  }
+}
